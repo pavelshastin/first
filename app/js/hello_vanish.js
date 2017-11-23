9fdd1b35659e59c62272caaceaffd414dda4b8d6
Pavel Shastin <pavelshastin@yandex.ru>
2017-11-23 14:18:04 +0300

    gradual vanishing of hello cover is added (javascript)

---
+++ app/js/hello_vanish.js
@@ -0,0 +1,15 @@
+﻿var time = 2000;
+var hello = document.getElementsByClassName("hello")[0];
+var hello_button_goto = document.getElementsByClassName('hello_button_goto')[0];
+
+
+hello_button_goto.onclick = function(){
+	
+	hello.style.opacity = 0;
+	console.log(this.className);
+		
+		setTimeout(function(){
+			hello.style.transition = "top 1s";
+			hello.style.top = -1000;
+		}, time);	
+}
