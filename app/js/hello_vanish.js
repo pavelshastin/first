725e373444a38eb84a254220d989f7b760b00ae7
Pavel Shastin <pavelshastin@yandex.ru>
2017-11-30 09:15:12 +0300

    mobile layout for -hello, -index pages were added. To prevent code duplication -about and -index pages is now created with index_base scss/jade files

--- app/js/hello_vanish.js
+++ app/js/hello_vanish.js
@@ -1,15 +1,28 @@
 ﻿var time = 2000;
 var hello = document.getElementsByClassName("hello")[0];
 var hello_button_goto = document.getElementsByClassName('hello_button_goto')[0];
+var footer = document.getElementsByClassName('footer')[0];
+var wrapper = document.getElementsByClassName('wrapper')[0];
 
+wrapper.style.opacity = 0;
+footer.style.opacity = 0;
 
 hello_button_goto.onclick = function(){
 	
 	hello.style.opacity = 0;
+
+	wrapper.style.opacity = 1;
+	footer.style.opacity = 1;
+
+	wrapper.style.transition = "opacity 2s";
+	footer.style.transition = "opacity 2s";
+	
 	console.log(this.className);
 		
+		
 		setTimeout(function(){
-			hello.style.transition = "top 1s";
+			hello.style.transition = "top 2s";
 			hello.style.top = -1000;
+
 		}, time);	
 }
